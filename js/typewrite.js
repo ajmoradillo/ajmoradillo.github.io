495b0ff514945729cd5c9f461c1f9e6ffc196063
ajmoradillo <49711070+ajmoradillo@users.noreply.github.com>
2019-05-07 18:36:21 +0800

    Add files via upload
    Initian Commit

---
+++ js/typewrite.js
@@ -0,0 +1,12 @@
+var typed3 = new Typed('.typewrite', {
+    strings: ['Hi! Im Ave Jay Moradillo', 'I love Programming!', 'Im a Team Player', 'Im Innovative'],
+    typeSpeed: 60,
+    backSpeed: 60,
+    smartBackspace: true, // this is a default
+    loop: true
+  });
+
+
+var modal = document.getElementById('myModal');
+var img = document.getElementById('myImg');
+
